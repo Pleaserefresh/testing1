7613fbca309ecb1d70a484135b0d1748fc559c65
jacekkudlanski <jacekkudlanski@gmail.com>
2020-11-16 23:02:43 +0100

    little update

--- src/com/jacode/Employee.java
+++ src/com/jacode/Employee.java
@@ -4,12 +4,17 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+     }
+
     public int calculateWage(int extraHours) {
 
         return baseSalary + (hourlyRate * extraHours);
     }
 
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be 0 or less.");
         this.baseSalary = baseSalary;
@@ -19,7 +24,7 @@ public class Employee {
         return baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if (hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly rate cannot be 0 or less.");
         this.hourlyRate = hourlyRate;
--- src/com/jacode/WageCalculator.java
+++ src/com/jacode/WageCalculator.java
@@ -3,9 +3,10 @@ package com.jacode;
 public class WageCalculator {
 
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
+        var employee = new Employee(50_000, 20);
+        employee.
+        //employee.setBaseSalary(50_000);
+        //employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
